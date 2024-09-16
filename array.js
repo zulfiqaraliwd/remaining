349c04933b10512d03cfc299bb35442e6e16bd71
Zulfiqar Ali <zulfiqaraliwd@gmail.com>
2024-09-16 15:05:17 +0500

    remaining function and array

---
+++ array.js
@@ -0,0 +1,189 @@
+//QUesstion no 01-----------------------------------------------------------------------------
+/*
+var arr = ["zulfiqar","abdullah","kazim"]
+console.log(arr);
+*/
+
+//QUesstion no 02-----------------------------------------------------------------------------
+
+/*
+var arr = new Array("zulfiqar","abdullah","kazim");
+console.log(arr)
+*/
+
+//QUesstion no 03-----------------------------------------------------------------------------
+
+/*var arr = ["zulfiaqr","ali","SMIT","web","developement"];
+console.log(arr);
+*/
+
+//QUesstion no 04-----------------------------------------------------------------------------
+
+/*
+var arr = [1,2,3,4,5,6,7];
+console.log(arr);
+*/
+
+//QUesstion no 05-----------------------------------------------------------------------------
+
+/*
+var arr= [true,false];
+console.log(arr)
+*/
+
+//QUesstion no 06-----------------------------------------------------------------------------
+
+/*
+var arr =["zulfiqar ali",12,true];
+console.log(arr);
+*/
+
+//QUesstion no 07-----------------------------------------------------------------------------
+
+/*
+var arr=["Telenor","Jazz","Ufone","Warid","Zong","Mobilink"];
+console.log(arr)
+*/
+
+//QUesstion no 08-----------------------------------------------------------------------------
+
+/*
+var numbers = [320, 230, 480, 120];
+
+var assending = numbers.sort();
+
+console.log(numbers);
+*/
+
+//QUesstion no 09---------------------------------------------------------------------------
+    /*
+    var arr = ["Avengers:age of ujrons","specture","jurassis world","inside out"]
+console.log("length of the array " + arr.length());
+*/
+
+//QUesstion no 10-----------------------------------------------------------------------------
+
+/*
+var arr = ["Audi","Volvo","Ford","Lanbarghoni"];
+console.log(arr.indexOf("Audi"));
+console.log(arr.indexOf("Lanbarghoni"))
+*/
+
+//QUesstion no 11-----------------------------------------------------------------------------
+
+/*
+var name = ["Michel","John","Tony"];
+var marks = ["64%","46%","96%"];
+
+console.log(name.)
+*/
+
+//QUesstion no 12-----------------------------------------------------------------------------
+
+
+/*
+        
+        var colors = ["Red", "Blue", "Green", "Yellow"];
+        console.log("<p>Initial Array: " + colors + "</p>");
+
+        
+        var Beginning = prompt("Enter a color to add to the beginning of the array:");
+        if (Beginning) {
+            colors.unshift(Beginning);
+        }
+        console.log("<p>After adding a color to the beginning: " + colors + "</p>");
+
+        var End = prompt("Enter a color to add to the end of the array:");
+        if (End) {
+            colors.push(End);
+        }
+        console.log("<p>After adding a color to the end: " + colors + "</p>");
+
+        colors.unshift("Pink", "Orange");
+        console.log("<p>After adding two colors to the beginning: " + colors + "</p>");
+
+
+
+
+        colors.shift();
+        console.log("<p>After deleting the first color: " + colors + "</p>");
+
+        
+        colors.pop();
+        console.log("<p>After deleting the last color: " + colors + "</p>");
+
+        var Add = parseInt(prompt("At which index do you want to add a color?"));
+        var Add = prompt("Enter the color to add at index " + Add);
+        if (Add && !isNaN(Add)) {
+            colors.splice(Add, 0, Add);
+        }
+        console.log("<p>After adding a color at index " + Add + ": " + colors + "</p>");
+
+        
+        var Delete = parseInt(prompt("At which index do you want to delete color(s)?"));
+        var toDelete = parseInt(prompt("How many colors do you want to delete?"));
+        if (!isNaN(toDelete) && !isNaN(toDelete)) {
+        colors.splice(Delete, toDelete); 
+        }
+        console.log("<p>After deleting " + toDelete + " color(s) from index " + Delete + ": " + colors + "</p>");
+
+    */
+
+
+
+//QUesstion no 13-----------------------------------------------------------------------------
+
+//QUesstion no 14-----------------------------------------------------------------------------
+
+/*
+var fruit = ["strawberry","apple","orange","banana"];
+
+console.log(fruit.sort());
+*/
+
+//QUesstion no 15-----------------------------------------------------------------------------
+
+/*
+var cities = ["karachi","lahore","islamabad","quetta"];
+var newCities=cities.slice(1,3);
+console.log(newCities);
+*/
+
+//QUesstion no 16-----------------------------------------------------------------------------
+
+/*
+var animal = ["this","is","my","cat"];
+var newAnimal= animal.join(',');
+console.log(newAnimal);
+*/
+
+
+//QUesstion no 17-----------------------------------------------------------------------------
+
+/*
+var devices= ["Keyboard", "Mouse", "Printer"];
+
+devices.push("Monitor"); 
+console.log(devices.shift());
+
+console.log(devices);       
+*/
+
+//QUesstion no 18-----------------------------------------------------------------------------
+
+
+/*
+var devices= ["Keyboard", "Mouse", "Printer"];
+
+devices.pop("Monitor"); 
+console.log(devices.shift());
+
+console.log(devices);       
+*/
+
+
+//QUesstion no 19-----------------------------------------------------------------------------
+
+/*multi ddimentiional array------------*/
+
+//QUesstion no 20-----------------------------------------------------------------------------
---
+++ function.js
@@ -0,0 +1,235 @@
+
+
+
+//Question no 01-------------------------------------------------------------------------------
+/*
+function start() {
+    console.log("hello world")
+}
+
+start();
+start();
+start();
+start();
+*/
+
+//Question no 03-------------------------------------------------------------------------------
+/*
+function greeting(first_name,last_name){
+console.log("Hello " + first_name , last_name)
+
+}
+
+greeting("zulfiqar" , "ali")
+*/
+
+//Question no 04--------------------------------------------------------------------------------
+
+
+/*
+function add() {
+    var num1 = parseFloat(prompt("Enter the first number:"));
+    var num2 = parseFloat(prompt("Enter the second number:"));
+
+    var sum = num1 + num2;
+    return sum;
+}
+var result =add();
+console.log(`The sum of the two numbers is: ${result}`);
+
+*/
+
+//Question no 05--------------------------------------------------------------------------------
+
+/*
+function DateTime() {
+    var currentDateTime = new Date();
+    console.log(currentDateTime);
+}
+
+DateTime();
+*/
+
+
+//Question no 06--------------------------------------------------------------------------------
+
+/*
+function sq(number) {
+    return number * number;
+}
+console.log(sq(2));
+console.log(sq(5));
+*/
+
+
+//Question no 07--------------------------------------------------------------------------------
+
+/*
+function factorial(n) {
+    if (n < 0) {
+        return "Factorial is not defined for negative numbers.";
+    } else if (n === 0 || n === 1) {
+        return 1;
+
+    } 
+    else {
+        let result = 1;
+        for (var i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
+console.log(factorial(5));
+console.log(factorial(0));
+console.log(factorial(-1));
+*/
+
+
+//Question no 08--------------------------------------------------------------------------------
+
+/*
+function counting(start, end) {
+    if (start > end) {
+        console.log("Start number should be less than or equal to end number.");
+        return;
+    }
+for (let i = start; i <= end; i++) {
+        console.log(i);
+    }
+}
+
+counting(1, 5); 
+*/
+
+//Question no 09--------------------------------------------------------------------------------
+
+
+/*
+function Hypotenuse(a, b) {
+
+    const c = Math.sqrt(a * a + b * b);
+    return c;
+}
+const sideA = +prompt("Enter the length of side A:");
+const sideB = +prompt("Enter the length of side B:");
+const hypotenuse = calculateHypotenuse(sideA, sideB);
+
+console.log(`The length of the hypotenuse is: ${hypotenuse}`);
+
+*/
+
+
+//Question no 10--------------------------------------------------------------------------------
+
+/*
+smj nh aya
+/*
+
+//Question no 11--------------------------------------------------------------------------------
+
+/*
+function LargestNumber(numbers) {
+    if (numbers.length === 0) {
+        return "No numbers provided.";
+    }
+
+    return Math.max(numbers);
+}
+console.log(LargestNumber(1, 5, 3, 9, 2));
+console.log(LargestNumber(10, 20, 30, 40));
+console.log(LargestNumber());
+*/
+
+
+
+//Question no 12--------------------------------------------------------------------------------
+
+/*
+function rec_Area(width, height) {
+    if (width <= 0 || height <= 0) {
+        return "Width and height must be positive numbers.";
+    }
+
+    return width * height;
+}
+console.log(rec_Area(5, 10)); 
+console.log(rec_Area(7, 3));  
+console.log(calculateRectangleArea(-5, 10));
+
+*/
+
+//Question no 13--------------------------------------------------------------------------------
+
+/*
+function Array(array) {
+    return array.sort((a, b) => a - b);
+}
+console.log(Array([5, 2, 9, 1, 5, 6]));
+*/
+
+
+
+//Question no 14--------------------------------------------------------------------------------
+
+/*smj nahi aya */
+
+
+//Question no 15--------------------------------------------------------------------------------
+
+/*
+var param = function inner(){
+return typeof inner;
+}
+alert(param());
+*/
+
+//Question no 16--------------------------------------------------------------------------------
+
+/*
+function power(base, exponent) {
+    return Math.pow(base, exponent);
+}
+
+console.log(power(2, 3)); 
+console.log(power(5, 2)); 
+
+*/
+
+
+//Question no 17--------------------------------------------------------------------------------
+
+
+
+
+
+//Question no 18--------------------------------------------------------------------------------
+
+
+
+
+//Question no 19--------------------------------------------------------------------------------
+
+
+
+//Question no 20--------------------------------------------------------------------------------
+
+
+
+//Question no 21--------------------------------------------------------------------------------
+
+
+
+//Question no 22--------------------------------------------------------------------------------
+
+
+
+//Question no 23--------------------------------------------------------------------------------
+
+
+
+//Question no 24--------------------------------------------------------------------------------
+
+
+
+//Question no 25--------------------------------------------------------------------------------
